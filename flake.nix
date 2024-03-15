e9fcb333658a368a9c861ffc096d4ab25fad45d5
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-03-15 14:45:14 -0300

    create flake for glypst development

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A flake for glypst development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.gleam
+          pkgs.erlang
+        ];
+      };
+    });
+}
